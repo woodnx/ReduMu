4596d10ebb9534338801e76e8ba8fb9729b6c431
woodnext <shun20020611@yahoo.co.jp>
2025-05-21 02:29:38 +0000

    fix typo

--- api/internal/domain/repository/task_repository.go
+++ api/internal/domain/repository/task_repository.go
@@ -6,7 +6,7 @@ import (
 	"github.com/woodnx/ReduMu/api/internal/domain/model"
 )
 
-//go:generate go run github.com/matryer/moq@latest -out ./mock_user_repository.go . ITaskRepo
+//go:generate go run github.com/matryer/moq@latest -out ./mock_task_repository.go . ITaskRepo
 type ITaskRepo interface {
 	Create(ctx context.Context, task *model.Task) error
 	FindByID(ctx context.Context, id model.TaskID) (*model.Task, error)
